e3192cea8f8896cc5abef40aa3143dc98aca931b
ridge <ridgerouzier@gmail.com>
2024-03-15 17:34:00 -0400

    add some attributes to file

---
+++ Building.java
@@ -0,0 +1,8 @@
+
+import java.util.LinkedList;
+public class Building {
+
+	LinkedList<Floor> floors = new LinkedList<Floor>();
+
+
+}
---
+++ Floor.java
@@ -0,0 +1,10 @@
+
+import java.util.ArrayList;
+
+public class Floor {
+
+	private ArrayList<Elevator> currentElevators = new ArrayList<Elevator>();
+
+	private ArrayList<Passenger> currentPassengers = new ArrayList<Passenger>();
+
+}
