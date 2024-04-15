32e1bc4f974944149705d6ea60db984f5b6c0291
ridge <ridgerouzier@gmail.com>
2024-04-15 15:41:04 -0400

    make animations

--- ElevatorPane.java
+++ ElevatorPane.java
@@ -1,13 +1,31 @@
 
+import javafx.animation.AnimationTimer;
 import javafx.scene.layout.Pane;
 public class ElevatorPane extends Pane {
 
+	AnimationTimer timer;
 public ElevatorPane(Building b) {
+
+
+
 	int num = b.getFloorNum();
 	
 	for(int i = 0; i < num; ++i) {
 		getChildren().add(b.floors.get(i).getFigure());
+
 	}
+		// anon class for animation
+
+	timer = new AnimationTimer() {
+	
+		public void handle(long t) {
+
+		}
+
+
+	};
+
+
 }
 
 }
