fd09798a8ff02fc9913968141fefed3ed4b02bba
ridge <ridgerouzier@gmail.com>
2024-04-15 15:41:37 -0400

    program client

--- Main.java
+++ Main.java
@@ -1,4 +1,10 @@
 
+/* try adding settings to building so building can get info
+ * put elevators in building, in a list
+ * traverse the elevators thorugh the linkedlist of floors
+ * express should take all staff before taking patients on floor
+ * elevator, passengers, floors, all initilized in building or maybe in Main with multiple params specifying building constructor
+ */
 
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -10,7 +16,7 @@ public class Main extends Application {
 
 public void start(Stage stage) {
 
-	Building b = new Building(s.numOfFloors,800,800,s.numStandard,s.numExpress);
+	Building b = new Building(20,800,800,10,10);
 	ElevatorPane p = new ElevatorPane(b);
 	stage.setScene(new Scene(p,700,700));
 	stage.show();
