d3515bb96c14ee43d54c35698a56c9a57a9d80a7
ridge <ridgerouzier@gmail.com>
2024-04-16 14:03:29 -0400

    put methods for elevator construction

--- Building.java
+++ Building.java
@@ -14,18 +14,26 @@ public LinkedList<Floor> floors;
 	public Settings s;
 	private int width, height;
 
-	public Building(int n,int w, int h, int sn, int en, double pRP, double sRP, int nP, int nS,int sC, int eC ) {
+	public Building(int n,int w, int h, int sn, int en, double pRP, double sRP, int nP, int nS,int sC, int eC,double stRP,double eRP ) {
 		floors = new LinkedList<Floor>();
 	
 		floorNum = n;
 		double fw = w/n;
 		double fh = h/n;
+		// puts floors in building
 		for(int i = 0; i < n; ++i) {
 			Floor temp = new Floor(i+1);
 			temp.setFigure(0,h-(i*fh),fw,fh);
 			floors.add(temp);
 		}
 
+		// adds patients and staff to floor
+		addPatient(nP,pRP);
+		addStaff(nS,sRP);
+
+		//addStandard(stRP,sC);
+
+		//addExpress(eRP,eC);
 
 	width = w;
 	height = h;
@@ -43,6 +51,27 @@ public LinkedList<Floor> floors;
 	}
 
 
+	// methods to add patients and staff to floors
+	private void addPatient(int num, double percent) {
+
+		for(int i = 0; i < num; ++i) {
+				Passenger p = new Patient(floors.get(i),floors.get((int)(Math.random()*floors.size())),percent);
+			floors.get(i).waitingQueue.add(p);
+			}
+		}
+
+	private void addStaff(int num, double percent) {
+
+		for(int i = 0; i < num; ++i) {
+				Passenger p = new Staff(floors.get(i),floors.get((int)(Math.random()*floors.size())),percent);
+			floors.get(i).waitingQueue.add(p);
+			}
+		}
 
 
-}
+		}
+	
+
+
+
+
