906b69b4214a08cda8e80df4d94872788c9bb3d4
ridge <ridgerouzier@gmail.com>
2024-04-15 23:28:24 -0400

    add methods to initilzie passengers and elevators, then make traversals

--- Building.java
+++ Building.java
@@ -1,19 +1,21 @@
 
+
 import java.util.LinkedList;
 import java.util.ArrayList;
+/* put some methods to initialize passengers in floors, and elevators in floors, then cook 
+ */
+
+
 public class Building {
 
 	// fields
 public LinkedList<Floor> floors;
-	private ArrayList<StandardElevator> standardElevators; 
-	private ArrayList<ExpressElevator> expressElevators; 
 	private int floorNum;
+	public Settings s;
 	private int width, height;
 
-	public Building(int n,int w, int h, int sn, int en) {
+	public Building(int n,int w, int h, int sn, int en, double pRP, double sRP, int nP, int nS,int sC, int eC ) {
 		floors = new LinkedList<Floor>();
-		standardElevators = new ArrayList<StandardElevator>();
-		expressElevators = new ArrayList<ExpressElevator>();
 	
 		floorNum = n;
 		double fw = w/n;
@@ -24,15 +26,6 @@ public LinkedList<Floor> floors;
 			floors.add(temp);
 		}
 
-
-		// fills building with elevators
-		int l = sn > en ? sn:en;
-		for(int i = 0; i < l; ++i) {
-			if(i < sn)
-				standardElevators.add(new StandardElevator(40,10,1));
-			if(i < en)
-				expressElevators.add(new ExpressElevator(20,10,1));
-		}
 
 	width = w;
 	height = h;
