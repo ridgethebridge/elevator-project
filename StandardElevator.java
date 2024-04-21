73ab7612fe1bad103ed38412d47e0450a46f0a61
ridge <ridgerouzier@gmail.com>
2024-04-21 01:19:06 -0400

    removed stuff

--- StandardElevator.java
+++ StandardElevator.java
@@ -1,39 +1,32 @@
 
 
-import java.util.ArrayList;
-
 public class StandardElevator extends Elevator {
-
-	private ArrayList<Passenger> passengerList = new ArrayList<Passenger>();
 
 	public StandardElevator(double requests, int capacity, Floor f) {
 		this.capacity = capacity;
 		requestPercentage = requests;
 		currentFloor = f;
+		direction = Direction.UP;
 	}
 
 
 
 //implement
-public boolean pickup(Passenger p) {
-	if(numPassenger < capacity) {
-		passengerList.add(p);
+public void pickup() {
+
+	for(int i = 0; i < currentFloor.waitingQueue.size(); ++i)
+		if(numPassenger < capacity) {
+		Passenger p = currentFloor.waitingQueue.get(i);
+		if((currentFloor.compare(p.getEndFloor()) < 0 && direction == Direction.UP) || (currentFloor.compare(p.getEndFloor())>0 && direction == Direction.DOWN)) {
+			passengerList.add(p);
+		currentFloor.waitingQueue.remove(p);
+		
 		++numPassenger;
-		return true;
 	}
-	return false;
+		}
 }
 
 
-
-//implement
-public Passenger drop(Passenger p) {
-	passengerList.remove(p);
-	--numPassenger;
-	return p;
-
-
-}
 }
 
 
