442cc61e27a66abde47f953aa8012a0e4a9cb781
ridge <ridgerouzier@gmail.com>
2024-04-24 15:52:58 -0400

    made universal constructor

--- Elevator.java
+++ Elevator.java
@@ -7,8 +7,22 @@ public abstract class Elevator {
 	protected int capacity;
 	protected int numPassenger = 0;
 	protected Floor currentFloor;
+	protected Floor destFloor;
+	protected boolean firstPicked;
 public Direction direction;
+
 protected ArrayList<Passenger> passengerList = new ArrayList<Passenger>();
+
+public Elevator( double r, int c, Floor f) {
+
+capacity = c;
+		requestPercentage = r;
+		currentFloor = f;
+		currentFloor.elevatorList.add(this);
+		direction = (int)(Math.random()*2) == 1 ? Direction.UP: Direction.DOWN;
+		firstPicked = false;
+}
+
 
 	public int getCapacity() {
 		return capacity;
@@ -37,5 +51,25 @@ protected ArrayList<Passenger> passengerList = new ArrayList<Passenger>();
 	currentFloor.elevatorList.remove(this);
 	currentFloor.setColor();
 	currentFloor = newFloor;
+	drop();
+	pickup();
+	currentFloor.setPassengerCounter();
+	resetFirstPicked();
+}
+
+public void resetFirstPicked() {
+	if (destFloor != null && currentFloor.equals(destFloor)) {
+		if(!passengerList.isEmpty()) {
+		destFloor = passengerList.get(0).getEndFloor();
+		}
+		else {
+			firstPicked = false;
+		}
+	}
+}
+
+
+public void setDirection(Floor f) {
+		direction  = currentFloor.compareTo(f) > 0 ? Direction.DOWN: Direction.UP;
 }
 }
