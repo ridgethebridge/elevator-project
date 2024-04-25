9b418a4ea6aec87f21fb39b3792948106d0345a1
ridge <ridgerouzier@gmail.com>
2024-04-25 09:53:26 -0400

    fixed bug

--- StandardElevator.java
+++ StandardElevator.java
@@ -14,7 +14,7 @@ public void pickup() {
 	for(int i = 0; i < currentFloor.waitingQueue.size(); ++i) {
 		Passenger p = currentFloor.waitingQueue.get(i);
 		if(numPassenger < capacity && p.getRequestChoice()) {
-		if((currentFloor.compareTo(p.getEndFloor()) > 0 && direction == Direction.UP) || (currentFloor.compareTo(p.getEndFloor())<0 && direction == Direction.DOWN)) {
+		if((currentFloor.compareTo(p.getEndFloor()) > 0 && direction == Direction.DOWN) || (currentFloor.compareTo(p.getEndFloor())<0 && direction == Direction.UP)) {
 			passengerList.add(p);
 		currentFloor.waitingQueue.remove(p);
 		
