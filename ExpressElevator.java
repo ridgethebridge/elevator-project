c0892193ddd9b78228529554d96970d85f9b52e5
ridge <ridgerouzier@gmail.com>
2024-04-25 11:25:34 -0400

    changed move

--- ExpressElevator.java
+++ ExpressElevator.java
@@ -1,13 +1,18 @@
 
 
+import javafx.scene.control.TextArea;
 public class ExpressElevator extends Elevator {
 
 public ExpressElevator(double r, int c, Floor f) {
 	super(r,c,f);
 }
 
+@Override
+public String toString() {
+	return "express elevator";
+}
 
-public void pickup() {
+public void pickup(TextArea a) {
 	// first looks for staff
 	for(int i = 0; i < currentFloor.waitingQueue.size(); ++i) {
 		Passenger p = currentFloor.waitingQueue.get(i);
@@ -16,6 +21,7 @@ public void pickup() {
 		if(p instanceof Staff) {
 			passengerList.add(p);
 			currentFloor.waitingQueue.remove(p);
+			a.setText(a.getText() + p + " was picked up by " + this + " on " + currentFloor +"\n");
 	++numPassenger;
 	if(!firstPicked) {
 		firstPicked = true;
@@ -37,6 +43,7 @@ public void pickup() {
 		if((currentFloor.compareTo(p.getEndFloor()) > 0 && direction == Direction.DOWN) || (currentFloor.compareTo(p.getEndFloor())<0 && direction == Direction.UP)) {
 			passengerList.add(p);
 			currentFloor.waitingQueue.remove(p);
+			a.setText(a.getText() + p + " was picked up by " + this + " on " + currentFloor +"\n");
 	++numPassenger;
 	if(!firstPicked) {
 		firstPicked = true;
